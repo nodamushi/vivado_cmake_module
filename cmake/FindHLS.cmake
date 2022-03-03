81132b2bcf5151a806bd5cb5677680cb1309f523
nodamushi <nodamushi@outlook.jp>
2022-03-03 11:18:16 +0900

    add comments

--- cmake/FindHLS.cmake
+++ cmake/FindHLS.cmake
@@ -1,3 +1,4 @@
+# Find Vitis HLS
 find_path(HLS_BIN_DIR
   vitis_hls
   PATHS ${VITIS_HLS_ROOT} ENV XILINX_HLS
@@ -10,20 +11,24 @@ find_path(HLS_INCLUDE_DIR
   PATH_SUFFIXES include
 )
 
+# save current directory
 set(HLS_CMAKE_DIR ${CMAKE_CURRENT_LIST_DIR})
 set(HLS_TCL_DIR ${CMAKE_CURRENT_LIST_DIR}/tcl)
 set(HLS_CMAKE_LIB_DIR ${CMAKE_CURRENT_BINARY_DIR}/lib)
 
+# hide variables
 mark_as_advanced(HLS_INCLUDE_DIR HLS_CMAKE_DIR HLS_TCL_DIR HLS_CMAKE_LIB_DIR)
 
-
+# find package
 include(FindPackageHandleStandardArgs)
 find_package_handle_standard_args(HLS
   REQUIRED_VARS
     HLS_BIN_DIR
     HLS_INCLUDE_DIR
-    HLS_TCL_DIR)
+    HLS_TCL_DIR
+)
 
+# provide HLS::HLS
 if(HLS_FOUND AND NOT TARGET HLS::HLS)
   add_library(HLS::HLS INTERFACE IMPORTED)
   set_target_properties(HLS::HLS
@@ -36,53 +41,74 @@ endif()
 get_filename_component(HLS_VERSION "${HLS_BIN_DIR}" DIRECTORY)
 get_filename_component(HLS_VERSION "${HLS_VERSION}" NAME)
 
-# デフォルトの CFLAGS
+# default clags option
 set(HLS_CFLAGS -Wall)
 
-
+# add_hls_project(
+#  <project>
+#  TOP      <top module>
+#  PERIOD   <clock period(ns)>
+#  PART     <board part>
+#  SOURCES  <C++ source file>...
+#  [INCDIRS    <include directory>...]
+#  [LINK       <link library>...]
+#  [TB_SOURCES <test bench C++ file>...]
+#  [TB_INCDIRS <include directory>...]
+#  [TB_LINK    <link libray>...]
+#  [DEPENDS    <depends target>...]
+#  [NAME    <display name>]
+#  [IPNAME  <IP name>]
+#  [VENDOR  <your name>]
+#  [TAXONOMY <category>]
+#  [VERSION  <version(x.y)>]
+#  [SOLUTION <solution name>]
+#  [COSIM_LDFLAGS <flag string>]
+#  [COSIM_TRACE_LEVEL <none|all|port|port_hier>]
+# )
+#
+# Define Targets:
+#   create_project_${project} : Create Vitis Project
+#   clear_${project}          : Delete Vitis project directory
+#   csynth_${project}         : Run synthesis
+#   cosim_${project}          : C/RTL simulation
+#   lib_${project}            : Compile C++
+#   test_${project}           : Compile TestBench
+#
+# Argument
+#  project: target name
+#
+# Taged Arguments
+#  TOP    : Top module name
+#  PERIOD : Clock Period (ns)
+#  PART   : Device part
+#  SOURCES: HLS source file
+#
+# Options
+#  NAME          : IP display name
+#  IPNAME        : IP name
+#  VENDOR        : Your name
+#  TAXONOMY      : IP category
+#  VERSION       : IP version(x.y)
+#  SOLUTION      : Solution name
+#  TB_SOURCES    : Test Bench source files
+#  INCDIRS       : Include directories
+#  TB_INCDIRS    : Include directories for test bench
+#  DEPENDS       : Dependency for create project
+#  LINK          : Link library
+#  TB_LINK     : Link library for testing
+#  COSIM_LDFLAGS : cosim_design -ldflags
+#  COSIM_TRACE_LEVEL: none, all, port, port_hier
+#
 function(add_hls_project project)
-  # Argument
-  #  project: target name
-  #    : targets
-  #    :    create_project_${project} : Create Vitis Project
-  #    :    csynth_${project} : Run synthesis
-  #    :    lib_${project} : Compile C++
-  #    :    test_${project}: Compile TestBench
-  #    :    cosim_${project}: C/RTL simulation
-  #    :    clear_${project}: Delete Vitis project directory
-  #
-  # Taged Arguments
-  #  TOP    : Top module name
-  #  PERIOD : Clock Period (ns)
-  #  PART   : Device part
-  #  SOURCES: HLS source file
-  #
-  # Options
-  #  NAME          : IP display name
-  #  IPNAME        : IP name
-  #  VENDOR        : Your name
-  #  TAXONOMY      : IP category
-  #  VERSION       : IP version(x.y)
-  #  SOLUTION      : Solution name
-  #  TB_SOURCES    : Test Bench source files
-  #  INCDIRS       : Include directories
-  #  TB_INCDIRS    : Include directories for test bench
-  #  DEPENDS       : Dependency for create project
-  #  LINK          : Link library
-  #  TEST_LINK     : Link library for testing
-  #  COSIM_LDFLAGS : cosim_design -ldflags
-  #  COSIM_TRACE_LEVEL: none, all, port, port_hier
-  #
   cmake_parse_arguments(
     HLS_ADD_PROJECT
     ""
-    # Must: TOP,PERIOD,PART
     "TOP;PERIOD;PART;SOLUTION;FLOW_TARGET;VERSION;DESCRIPTION;NAME;IPNAME;TAXONOMY;VENDOR;COSIM_TRACE_LEVEL"
-    # Must: SOURCES
-    "SOURCES;TB_SOURCES;INCDIRS;TB_INCDIRS;DEPENDS;LINK;TEST_LINK;COSIM_LDFLAGS"
+    "SOURCES;TB_SOURCES;INCDIRS;TB_INCDIRS;DEPENDS;LINK;TB_LINK;COSIM_LDFLAGS"
     ${ARGN}
   )
 
+  # Check arguments
   if(NOT HLS_ADD_PROJECT_TOP)
     message(FATAL_ERROR "add_hls_project: TOP (top module name) is not defined.")
   endif()
@@ -99,9 +125,11 @@ function(add_hls_project project)
     message(FATAL_ERROR "add_hls_project: SOURCES (HLS source file) is not defined.")
   endif()
 
+  # set default option value
   if(NOT HLS_ADD_PROJECT_VERSION)
     set(HLS_ADD_PROJECT_VERSION "0.0")
   endif()
+  #  * Version string separated by underscores
   string(REPLACE "." "_" HLS_ADD_PROJECT_VERSION_ ${HLS_ADD_PROJECT_VERSION})
 
   if(NOT HLS_ADD_PROJECT_DESCRIPTION)
@@ -111,7 +139,6 @@ function(add_hls_project project)
   if(NOT HLS_ADD_PROJECT_NAME)
     set(HLS_ADD_PROJECT_NAME ${project})
   endif()
-
 
   if(NOT HLS_ADD_PROJECT_VENDOR)
   set(HLS_ADD_PROJECT_VENDOR "Anonymous")
@@ -125,8 +152,19 @@ function(add_hls_project project)
     set(HLS_ADD_PROJECT_TAXONOMY "Virus")
   endif()
 
+  if(NOT HLS_ADD_PROJECT_SOLUTION)
+    set(HLS_ADD_PROJECT_SOLUTION "solution1")
+  endif()
 
+  if(NOT HLS_ADD_PROJECT_FLOW_TARGET)
+    set(HLS_ADD_PROJECT_FLOW_TARGET "vivado")
+  endif()
 
+  if(NOT HLS_ADD_PROJECT_COSIM_TRACE_LEVEL)
+    set(HLS_ADD_PROJECT_COSIM_TRACE_LEVEL port_hier)
+  endif()
+
+  # fix relative path
   set(HLS_ADD_PROJECT_SOURCES_0)
   foreach(HLS_ADD_PROJECT_SRC IN LISTS HLS_ADD_PROJECT_SOURCES)
     if (IS_ABSOLUTE ${HLS_ADD_PROJECT_SRC})
@@ -145,15 +183,7 @@ function(add_hls_project project)
     endif()
   endforeach()
 
-
-  if(NOT HLS_ADD_PROJECT_SOLUTION)
-    set(HLS_ADD_PROJECT_SOLUTION "solution1")
-  endif()
-
-  if(NOT HLS_ADD_PROJECT_FLOW_TARGET)
-    set(HLS_ADD_PROJECT_FLOW_TARGET "vivado")
-  endif()
-
+  # create -I** option
   set(HLS_ADD_PROJECT_CFLAG ${HLS_CFLAGS} -I${CMAKE_CURRENT_SOURCE_DIR})
   foreach(HLS_ADD_PROJECT_INCDIR IN LISTS HLS_ADD_PROJECT_INCDIRS)
     if (IS_ABSOLUTE ${HLS_ADD_PROJECT_INCDIR})
@@ -181,14 +211,7 @@ function(add_hls_project project)
     endif()
   endforeach()
 
-  if(NOT HLS_ADD_PROJECT_COSIM_TRACE_LEVEL)
-    set(HLS_ADD_PROJECT_COSIM_TRACE_LEVEL port_hier)
-  endif()
-
-
-  set(HLS_ADD_PROJECT_DIR ${CMAKE_CURRENT_BINARY_DIR}/${project})
-  set(HLS_ADD_PROJECT_PROJECT ${HLS_ADD_PROJECT_DIR}/${project}.app)
-
+  # define compile target
   add_library(lib_${project} STATIC ${HLS_ADD_PROJECT_SOURCES})
   target_link_libraries(lib_${project}
     PUBLIC
@@ -200,13 +223,14 @@ function(add_hls_project project)
       ${HLS_ADD_PROJECT_INCDIRS}
   )
 
+  # define test-bench compile target
   if(HLS_ADD_PROJECT_TB_SOURCES)
     add_executable(test_${project}
         ${HLS_ADD_PROJECT_TB_SOURCES}
     )
     target_link_libraries(test_${project}
       PUBLIC
-        ${HLS_ADD_PROJECT_TEST_LINK}
+        ${HLS_ADD_PROJECT_TB_LINK}
         lib_${project}
     )
     target_include_directories(test_${project}
@@ -215,17 +239,18 @@ function(add_hls_project project)
     )
   endif()
 
-
-
-  add_custom_target(create_project_${project}
-  SOURCES ${HLS_ADD_PROJECT_PROJECT})
-
+  # replace ";" -> " " for tcl scripts
   string(REPLACE ";" " " HLS_ADD_PROJECT_SOURCES_0 "${HLS_ADD_PROJECT_SOURCES_0}")
   string(REPLACE ";" " " HLS_ADD_PROJECT_TB_SOURCES_0 "${HLS_ADD_PROJECT_TB_SOURCES_0}")
   string(REPLACE ";" " " HLS_ADD_PROJECT_CFLAG "${HLS_ADD_PROJECT_CFLAG}")
   string(REPLACE ";" " " HLS_ADD_PROJECT_TB_CFLAG "${HLS_ADD_PROJECT_TB_CFLAG}")
   string(REPLACE ";" " " HLS_ADD_PROJECT_COSIM_LDFLAGS "-L${HLS_CMAKE_LIB_DIR};${HLS_ADD_PROJECT_COSIM_LDFLAGS}")
 
+  # define vitis hls project target
+  set(HLS_ADD_PROJECT_DIR ${CMAKE_CURRENT_BINARY_DIR}/${project})
+  set(HLS_ADD_PROJECT_PROJECT ${HLS_ADD_PROJECT_DIR}/${project}.app)
+
+  add_custom_target(create_project_${project} SOURCES ${HLS_ADD_PROJECT_PROJECT})
   add_custom_command(
     OUTPUT ${HLS_ADD_PROJECT_PROJECT}
     DEPENDS ${HLS_ADD_PROJECT_DEPENDS}
@@ -245,8 +270,8 @@ function(add_hls_project project)
       ${HLS_BIN_DIR}/vitis_hls ${HLS_TCL_DIR}/create_vitis_HLS_project.tcl
   )
 
+  # synthesis target
   set(HLS_ADD_PROJECT_CSYNTH_ZIP ${HLS_ADD_PROJECT_DIR}/${project}/${HLS_ADD_PROJECT_SOLUTION}/impl/ip/${HLS_ADD_PROJECT_VENDER}_hls_${HLS_ADD_PROJECT_TOP}_${HLS_ADD_PROJECT_VERSION_}.zip)
-
   add_custom_target(csynth_${project} SOURCES ${HLS_ADD_PROJECT_CSYNTH_ZIP})
   add_custom_command(OUTPUT ${HLS_ADD_PROJECT_CSYNTH_ZIP}
     DEPENDS create_project_${project}
@@ -264,6 +289,7 @@ function(add_hls_project project)
       ${HLS_BIN_DIR}/vitis_hls ${HLS_TCL_DIR}/csynth.tcl
   )
 
+  # C/RTL simulation target
   add_custom_target(cosim_${project}
     DEPENDS csynth_${project}
     COMMAND
@@ -276,6 +302,7 @@ function(add_hls_project project)
       ${HLS_BIN_DIR}/vitis_hls ${HLS_TCL_DIR}/cosim.tcl
   )
 
+  # delete project target
   add_custom_target(clear_${project}
     COMMAND ${CMAKE_COMMAND} -E remove_directory ${HLS_ADD_PROJECT_DIR}
   )
--- cmake/FindVivado.cmake
+++ cmake/FindVivado.cmake
@@ -1,16 +1,19 @@
+# find vivado
 find_path(VIVADO_BIN_DIR
   vivado
   PATHS ${VIVADO_ROOT} ENV XILINX_VIVADO
   PATH_SUFFIXES bin
 )
 
+# save currrent directory
 set(VIVADO_CMAKE_DIR ${CMAKE_CURRENT_LIST_DIR})
 set(VIVADO_TCL_DIR ${CMAKE_CURRENT_LIST_DIR}/tcl)
 set(VIVADO_CMAKE_BINARY_DIR ${CMAKE_CURRENT_BINARY_DIR})
 
+# hide variable
 mark_as_advanced(VIVADO_BIN_DIR VIVADO_CMAKE_DIR VIVADO_TCL_DIR)
 
-
+# find package
 include(FindPackageHandleStandardArgs)
 find_package_handle_standard_args(Vivado
   REQUIRED_VARS
@@ -21,6 +24,7 @@ find_package_handle_standard_args(Vivado
 get_filename_component(VIVADO_VERSION "${VIVADO_BIN_DIR}" DIRECTORY)
 get_filename_component(VIVADO_VERSION "${VIVADO_VERSION}" NAME)
 
+# xsdb target
 if(WIN32)
   add_custom_target(xsdb
     COMMAND ${VIVADO_BIN_DIR}/xsdb.bat
@@ -31,29 +35,40 @@ else()
   )
 endif()
 
+# add_vivado_project(
+#    <project>
+#    BOARD <board name>
+#    TOP   <top module>
+#    [DIR <directory name>]
+#    [RTL <file/directory>...]
+#    [CONSTRAINT <file/directory>...]
+#    [IP <directory>...]
+#    [DESIGN <tcl file>]
+#    [DEPENDS <target>...]
+# )
+#
+# Define Targets:
+#  ${project}                : Create Vivado project
+#  clear_${project}          : Delete Vivado project directory
+#  impl_${project}           : Create bit stream
+#   _impl_${project}_original: Run vivado to genenarate bitstream
+#
+# Argument:
+#  project: target name
+#
+# Taged Arguments
+#  BOARD  : board property
+#  TOP    : Top module name
+#
+# Options
+#  DIR        : project directory name (default is ${projet}.prj)
+#  RTL        : RTL files
+#  CONSTRAINT : constraint files
+#  IP         : IP directories
+#  DESIGN     : Design tcl file
+#  DEPENDS    : depends
+#
 function(add_vivado_project project)
-  # Argument
-  #  project: target name
-  #    : targets
-  #    :    create_project_${project} : Create Vitis Project
-  #    :    csynth_${project} : Run synthesis
-  #    :    lib_${project} : Compile C++
-  #    :    test_${project}: Compile TestBench
-  #    :    cosim_${project}: C/RTL simulation
-  #    :    clear_${project}: Delete Vitis project directory
-  #
-  # Taged Arguments
-  #  BOARD  : board property
-  #  TOP    : Top module name
-  #
-  # Options
-  #  DIR        : project directory name (default is ${projet}.prj)
-  #  RTL        : RTL files
-  #  CONSTRAINT : constraint files
-  #  IP         : IP directories
-  #  DESIGN     : Design tcl file
-  #  DEPENDS    : depends
-  #
   cmake_parse_arguments(
     VIVADO_ADD_PROJECT
     ""
@@ -62,6 +77,7 @@ function(add_vivado_project project)
     ${ARGN}
   )
 
+  # Check arguments
   if(NOT VIVADO_ADD_PROJECT_BOARD)
     message(FATAL_ERROR "add_vivado_project: BOARD is not defined.")
   endif()
@@ -70,10 +86,12 @@ function(add_vivado_project project)
     message(FATAL_ERROR "add_vivado_project: TOP (top module name) is not defined.")
   endif()
 
+  # set default option value
   if(NOT VIVADO_ADD_PROJECT_DIR)
     set(VIVADO_ADD_PROJECT_DIR "${project}.prj")
   endif()
 
+  # fix relative path
   set(VIVADO_ADD_PROJECT_RTL_0)
   foreach(VIVADO_ADD_PROJECT_PATH IN LISTS VIVADO_ADD_PROJECT_RTL)
     if (IS_ABSOLUTE ${VIVADO_ADD_PROJECT_PATH})
@@ -100,22 +118,22 @@ function(add_vivado_project project)
       list(APPEND VIVADO_ADD_PROJECT_IP_0 ${CMAKE_CURRENT_SOURCE_DIR}/${VIVADO_ADD_PROJECT_PATH})
     endif()
   endforeach()
+
   if(VIVADO_ADD_PROJECT_DESIGN)
     if (NOT IS_ABSOLUTE ${VIVADO_ADD_PROJECT_DESIGN})
       set(VIVADO_ADD_PROJECT_DESIGN ${CMAKE_CURRENT_SOURCE_DIR}/${VIVADO_ADD_PROJECT_DESIGN})
     endif()
   endif()
 
-  set(VIVADO_ADD_PROJECT_DIR_0 ${CMAKE_CURRENT_BINARY_DIR}/${VIVADO_ADD_PROJECT_DIR})
-  set(VIVADO_ADD_PROJECT_PROJECT ${VIVADO_ADD_PROJECT_DIR_0}/${project}.xpr)
-
-
-  add_custom_target(${project} SOURCES ${VIVADO_ADD_PROJECT_PROJECT})
-
+  # replace ";" -> " " for tcl scripts
   string(REPLACE ";" " " VIVADO_ADD_PROJECT_RTL_0        "${VIVADO_ADD_PROJECT_RTL_0}")
   string(REPLACE ";" " " VIVADO_ADD_PROJECT_IP_0         "${VIVADO_ADD_PROJECT_IP_0}")
   string(REPLACE ";" " " VIVADO_ADD_PROJECT_CONSTRAINT_0 "${VIVADO_ADD_PROJECT_CONSTRAINT_0}")
 
+  # define ${project} target(create Vivado project)
+  set(VIVADO_ADD_PROJECT_DIR_0 ${CMAKE_CURRENT_BINARY_DIR}/${VIVADO_ADD_PROJECT_DIR})
+  set(VIVADO_ADD_PROJECT_PROJECT ${VIVADO_ADD_PROJECT_DIR_0}/${project}.xpr)
+  add_custom_target(${project} SOURCES ${VIVADO_ADD_PROJECT_PROJECT})
   add_custom_command(
     OUTPUT ${VIVADO_ADD_PROJECT_PROJECT}
     DEPENDS ${VIVADO_ADD_PROJECT_DEPENDS}
@@ -136,19 +154,18 @@ function(add_vivado_project project)
           ${VIVADO_ADD_PROJECT_DIR_0}
   )
 
+  # synthesis,impl,gen bitstream target
   set(VIVADO_ADD_PROJECT_BIT ${VIVADO_ADD_PROJECT_DIR_0}/${project}.runs/impl_1/${VIVADO_ADD_PROJECT_TOP}.bit)
   set(VIVADO_ADD_PROJECT_BIT_COPY ${VIVADO_CMAKE_BINARY_DIR}/bit/${project}.bit)
-
-  add_custom_target(_impl_${project}_original
-    SOURCES ${VIVADO_ADD_PROJECT_BIT})
-
+  #    Run  vivado   : _impl_${project}_original target
+  #    Copy bitstream: impl_${project} target
+  add_custom_target(_impl_${project}_original SOURCES ${VIVADO_ADD_PROJECT_BIT})
   add_custom_target(impl_${project}
     DEPENDS _impl_${project}_original
     COMMAND ${CMAKE_COMMAND} -E copy ${VIVADO_ADD_PROJECT_BIT} ${VIVADO_ADD_PROJECT_BIT_COPY}
     COMMAND ${CMAKE_COMMAND} -E copy ${VIVADO_ADD_PROJECT_DIR_0}/${project}.runs/impl_1/${VIVADO_ADD_PROJECT_TOP}.* ${VIVADO_CMAKE_BINARY_DIR}/bit/
     COMMAND echo "Output BitStream: ${VIVADO_ADD_PROJECT_BIT} ${VIVADO_ADD_PROJECT_BIT_COPY}"
   )
-
   add_custom_command(
     OUTPUT ${VIVADO_ADD_PROJECT_BIT}
     DEPENDS ${project}
@@ -162,6 +179,7 @@ function(add_vivado_project project)
           ${VIVADO_ADD_PROJECT_DIR}
   )
 
+  # delete project target
   add_custom_target(clear_${project}
     COMMAND ${CMAKE_COMMAND} -E remove_directory ${VIVADO_ADD_PROJECT_DIR_0}
     COMMAND ${CMAKE_COMMAND} -E remove ${CMAKE_CURRENT_BINARY_DIR}/vivado*
