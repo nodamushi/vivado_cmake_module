7701692136c29ce84bc050f111b4a1bf6c78b808
nodamushi <nodamushi@outlook.jp>
2022-03-07 13:49:01 +0900

    fix export_bd.tcl

--- cmake/tcl/export_bd.tcl
+++ cmake/tcl/export_bd.tcl
@@ -9,15 +9,18 @@ set tcl_directory     [lindex $argv 2]
 set output_tcl_file   [lindex $argv 3]
 set design_name       [lindex $argv 4]
 
-# get bd file
 source $tcl_directory/_source_find_bd.tcl
+
+
+puts "${project_directory}/${project_name}.xpr"
+puts [pwd]
+
+open_project ${project_directory}/${project_name}.xpr
 set bd_file [getBdFile "${project_directory}/${project_name}.srcs/sources_1/bd" $design_name]
 if { "$bd_file" == "" } {
   exit 1
 }
 
-
-open_project ${project_directory}/${project_name}.xpr
 open_bd_design $bd_file
 write_bd_tcl -force $output_tcl_file
 close_project
